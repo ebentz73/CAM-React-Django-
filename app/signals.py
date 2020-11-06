bb5096aea8d0fd49a1866ef70ed98134159f9ca3
Mitchell Marsden <mmarsden@lone-star.com>
2020-11-06 10:19:25 -0600

    Improve efficiency of sqlite querying

--- app/signals.py
+++ app/signals.py
@@ -4,10 +4,24 @@ import tempfile
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 
-from app.models import (AnalyticsSolution, FilterCategory,
-                        FilterOption, InputDataSet, InputPage,
-                        Model, Scenario, Node, InputNodeData, ConstNodeData)
-from app.proto_modules import TagFilterOption_pb2, InputNodeData_pb2, NodeTagListBlob_pb2, ConstNodeData_pb2
+from app.models import (
+    AnalyticsSolution,
+    ConstNodeData,
+    FilterCategory,
+    FilterOption,
+    InputDataSet,
+    InputNodeData,
+    InputPage,
+    Model,
+    Node,
+    Scenario,
+)
+from app.proto_modules import (
+    ConstNodeData_pb2,
+    InputNodeData_pb2,
+    NodeTagListBlob_pb2,
+    TagFilterOption_pb2,
+)
 from app.utils import Sqlite
 
 
@@ -16,7 +30,7 @@ def update_model(sender, **kwargs):
     solution = kwargs.get('instance')
 
     if 'tam_file' in solution.changed_fields:
-        # Download tam model file from GCS and save what we need out of it
+        # Download tam model file and save only what we need
         f, filename = tempfile.mkstemp()
         try:
             os.write(f, solution.tam_file.read())
@@ -24,72 +38,107 @@ def update_model(sender, **kwargs):
 
             # Open sqlite file
             with Sqlite(filename) as cursor:
-                cursor.execute("select * from FilterCategories")
-                for record in cursor.fetchall():
-                    #
+                # Save all filters
+                cursor.execute(
+                    "SELECT CategoryName, FilterOptionsBlob FROM FilterCategories"
+                )
+                for category_name, filter_blob in cursor.fetchall():
+                    category, _ = FilterCategory.objects.update_or_create(
+                        solution=solution, name=category_name
+                    )
                     blob_model = TagFilterOption_pb2.List_TagFilterOption()
-                    blob_model.ParseFromString(record[2])
-                    category_name = record[1]
-                    category, _ = FilterCategory.objects.update_or_create(solution=solution, name=category_name)
-                    for option in blob_model.items._values:
-                        option, _ = FilterOption.objects.update_or_create(category=category, tag=option.Tag,
-                                                                          display_name=option.DisplayName)
-                cursor.execute('select * from TruNavModel')
-                for model_record in cursor.fetchall():
-                    model_id = model_record[1]
-                    model_name = model_record[2]
-                    model, _ = Model.objects.update_or_create(solution=solution, tam_id=model_id,
-                                                              defaults={'name': model_name})
+                    blob_model.ParseFromString(filter_blob)
+                    for option in blob_model.items:
+                        FilterOption.objects.update_or_create(
+                            category=category,
+                            tag=option.Tag,
+                            display_name=option.DisplayName,
+                        )
+
+                # Save all models
+                cursor.execute("SELECT ModelId, ModelName FROM TruNavModel")
+                for model_id, model_name in cursor.fetchall():
+                    model, _ = Model.objects.update_or_create(
+                        solution=solution,
+                        tam_id=model_id,
+                        defaults={'name': model_name},
+                    )
 
                     # Save all input pages
-                    cursor.execute('select * from ModelDataPage where ModelId=? and pagetype=0', (model_id,))
-                    for data_page_record in cursor.fetchall():
-                        input_page_id = data_page_record[2]
-                        input_page_name = data_page_record[3]
-                        InputPage.objects.update_or_create(model=model, tam_id=input_page_id,
-                                                           defaults={'name': input_page_name})
+                    cursor.execute(
+                        "SELECT PageId, PageName "
+                        "FROM ModelDataPage "
+                        "WHERE ModelId=? AND pagetype=0",
+                        (model_id,),
+                    )
+                    for page_id, page_name in cursor.fetchall():
+                        InputPage.objects.update_or_create(
+                            model=model, tam_id=page_id, defaults={'name': page_name}
+                        )
 
                     # Save all nodes
-                    cursor.execute('select * from Node where ModelId=?', (model_id,))
-                    for node_record in cursor.fetchall():
-                        node_id = node_record[2]
-                        node_name = node_record[3]
-                        node_type = node_record[5]
+                    cursor.execute(
+                        "SELECT n.NodeId, NodeName, NodeType, TagList, NodeInputData "
+                        "FROM Node AS n, NodeScenarioData AS d "
+                        "WHERE n.NodeId = d.NodeId "
+                        "   AND n.ModelId=? "
+                        "   AND NodeType IN ('inputnode', 'constnode')",
+                        (model_id,),
+                    )
+                    for (
+                        node_id,
+                        node_name,
+                        node_type,
+                        tag_list_blob,
+                        node_data_blob,
+                    ) in cursor.fetchall():
+                        # Get tags
                         blob_model = NodeTagListBlob_pb2.List_String()
-                        blob_model.ParseFromString(node_record[15])
-                        tag_list = blob_model.items._values
-                        is_right_type = node_type in ('inputnode', 'constnode', 'inputiteratornode', 'percentAllocationNode')
+                        blob_model.ParseFromString(tag_list_blob)
+                        tag_list = list(blob_model.items)
 
                         # Check if node has CAM tags
-                        has_cam_tags = False
-                        for tag in tag_list:
-                            has_cam_tags = has_cam_tags or tag.startswith('CAM_INPUT_CATEGORY==')
-                            if has_cam_tags:
-                                break
-                        if is_right_type and has_cam_tags:
+                        has_tags = any(True for tag in tag_list if tag.startswith('CAM_'))
+
+                        # Only save nodes if they are used by CAM
+                        if has_tags:
                             # Create Node model
-                            node, _ = Node.objects.update_or_create(model=model, tags=tag_list,
-                                                                    tam_id=node_id, defaults={'name': node_name})
+                            node, _ = Node.objects.update_or_create(
+                                model=model,
+                                tags=tag_list,
+                                tam_id=node_id,
+                                defaults={'name': node_name},
+                            )
+
                             # Create InputNodeData model
                             if node_type == 'inputnode':
-                                cursor.execute("select NodeInputData from NodeScenarioData"
-                                               " where NodeId=?", (node_id,))
                                 blob_model = InputNodeData_pb2.InputNodeData()
-                                record = cursor.fetchone()
-                                blob_model.ParseFromString(record[0])
-                                node_data = [[val.InputData.LowerBound, val.InputData.Low, val.InputData.Nominal,
-                                              val.InputData.High, val.InputData.UpperBound]
-                                             for val in blob_model.LayerData._values]
-                                InputNodeData.objects.update_or_create(node=node, default_data=node_data, is_model=True)
+                                blob_model.ParseFromString(node_data_blob)
+                                node_data = [
+                                    [
+                                        val.InputData.LowerBound,
+                                        val.InputData.Low,
+                                        val.InputData.Nominal,
+                                        val.InputData.High,
+                                        val.InputData.UpperBound,
+                                    ]
+                                    for val in blob_model.LayerData
+                                ]
+                                InputNodeData.objects.update_or_create(
+                                    node=node, default_data=node_data, is_model=True
+                                )
 
                             # Create ConstNodeData model
-                            if node_type == 'constnode':
-                                cursor.execute("select NodeInputData from NodeScenarioData where NodeId=?", (node_id,))
-                                record = cursor.fetchone()
+                            elif node_type == 'constnode':
                                 blob_model = ConstNodeData_pb2.ConstNodeData()
-                                blob_model.ParseFromString(record[0])
-                                node_data = [val.ConstData for val in blob_model.AllLayerData._values]
-                                ConstNodeData.objects.update_or_create(node=node, default_data=node_data, is_model=True)
+                                blob_model.ParseFromString(node_data_blob)
+                                node_data = [
+                                    val.ConstData
+                                    for val in blob_model.AllLayerData
+                                ]
+                                ConstNodeData.objects.update_or_create(
+                                    node=node, default_data=node_data, is_model=True
+                                )
 
         finally:
             os.remove(filename)
@@ -104,7 +153,10 @@ def input_data_set_scenario_changed(sender, **kwargs):
     if action == 'pre_add':
         for scenario_id in pk_set:
             scenario = Scenario.objects.get(pk=scenario_id)
-            for input_page_id in scenario.input_data_sets.values_list('input_page_id', flat=True):
+            for input_page_id in scenario.input_data_sets.values_list(
+                'input_page_id', flat=True
+            ):
                 if ids.input_page.id == input_page_id:
                     raise Exception(
-                        f"Scenario '{scenario.name}' cannot have multiple input data sets associated with Input Page '{ids.input_page.name}'.")
+                        f"Scenario '{scenario.name}' cannot have multiple input data sets associated with Input Page '{ids.input_page.name}'."
+                    )
