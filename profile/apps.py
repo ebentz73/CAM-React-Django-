7f0938bb18a45064baef0b76763101ca28ad6cfd
Mitchell Marsden <mmarsden@lone-star.com>
2020-12-01 10:50:50 -0600

    Add signal to create user profile and register profile in admin

--- profile/apps.py
+++ profile/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class MyAppConfig(AppConfig):
     name = 'profile'
+
+    # noinspection PyUnresolvedReferences
+    def ready(self):
+        import profile.signals
