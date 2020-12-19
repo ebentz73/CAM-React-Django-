428618a15dcb419cae6089b146076ccddbc0d516
ebentz73 <evanbentz.dev@gmail.com>
2020-12-19 09:05:23 -0500

    CAM-207: add pagenot found page

--- frontend-app/src/components/app.js
+++ frontend-app/src/components/app.js
@@ -1,45 +1,57 @@
-import React, {Component} from "react";
-import {render} from "react-dom";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScenarioDefinitionPage from "./ScenarioDefinitionPage";
-import PowerBIReport from './PowerBIReport';
-import AnalyticsSolutionPage from './AnalyticsSolutionPage';
-import ScenarioHomePage from './ScenarioHomePage';
-import LoginPage from './LoginPage';
+import PowerBIReport from "./PowerBIReport";
+import AnalyticsSolutionPage from "./AnalyticsSolutionPage";
+import ScenarioHomePage from "./ScenarioHomePage";
+import LoginPage from "./LoginPage";
+import PageNotFound from "./PageNotFound";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-            <div className="trunav-main">
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path='/frontend-app/solution/:id/new-scenario' component={ScenarioDefinitionPage}/>
-                    </Switch>
-                    <Switch>
-                        <Route exact path='/frontend-app/solution/:id/scenario/:scenarioId' component={ScenarioDefinitionPage}/>
-                    </Switch>
-                    <Switch>
-                        <Route exact path='/frontend-app/solution/:id/scenario' component={ScenarioHomePage}/>
-                    </Switch>
-                    <Switch>
-                        <Route exact path='/frontend-app/home' component={AnalyticsSolutionPage}/>
-                    </Switch>
-                    <Switch>
-                        <Route exact path='/frontend-app' component={ScenarioDefinitionPage}/>
-                    </Switch>
-                    <Switch>
-                        <Route exact path='/frontend-app/login' component={LoginPage}/>
-                    </Switch>
-                </BrowserRouter>
-            </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="trunav-main">
+          <BrowserRouter>
+            <Switch>
+              <Route
+                exact
+                path="/frontend-app/solution/:id/new-scenario"
+                component={ScenarioDefinitionPage}
+              />
+              <Route
+                exact
+                path="/frontend-app/solution/:id/scenario/:scenarioId"
+                component={ScenarioDefinitionPage}
+              />
+              <Route
+                exact
+                path="/frontend-app/solution/:id/scenario"
+                component={ScenarioHomePage}
+              />
+              <Route
+                exact
+                path="/frontend-app/home"
+                component={AnalyticsSolutionPage}
+              />
+              <Route
+                exact
+                path="/frontend-app"
+                component={ScenarioDefinitionPage}
+              />
+              <Route exact path="/frontend-app/login" component={LoginPage} />
+              <Route component={PageNotFound} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
