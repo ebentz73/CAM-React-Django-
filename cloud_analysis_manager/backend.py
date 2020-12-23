7e6dda0ec95f20fdea15231008be9942a7628413
Mitchell Marsden <mmarsden@lone-star.com>
2020-12-23 15:07:14 -0600

    Set custom domain for cloud backends

--- cloud_analysis_manager/backend.py
+++ cloud_analysis_manager/backend.py
@@ -3,14 +3,16 @@ from storages.backends.azure_storage import AzureStorage
 
 
 class AzureMediaStorage(AzureStorage):
+    custom_domain = settings.AZ_STORAGE_HOST
     account_name = settings.AZ_STORAGE_ACCOUNT
     account_key = settings.AZ_STORAGE_KEY
     azure_container = settings.AZ_MEDIA_CONTAINER
-    expiration_secs = None
+    expiration_secs = 600
     overwrite_files = True
 
 
 class AzureStaticStorage(AzureStorage):
+    custom_domain = settings.AZ_STORAGE_HOST
     account_name = settings.AZ_STORAGE_ACCOUNT
     account_key = settings.AZ_STORAGE_KEY
     azure_container = settings.AZ_STATIC_CONTAINER
