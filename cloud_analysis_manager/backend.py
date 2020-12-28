43f66ba129f7c256b9ca3013ddb5174d79c9e337
Mitchell Marsden <mmarsden@lone-star.com>
2020-12-28 11:00:33 -0600

    Set custom domain and endpoint suffix for azure storage

--- cloud_analysis_manager/backend.py
+++ cloud_analysis_manager/backend.py
@@ -3,17 +3,19 @@ from storages.backends.azure_storage import AzureStorage
 
 
 class AzureMediaStorage(AzureStorage):
-    custom_domain = settings.AZ_STORAGE_HOST
     account_name = settings.AZ_STORAGE_ACCOUNT
     account_key = settings.AZ_STORAGE_KEY
     azure_container = settings.AZ_MEDIA_CONTAINER
+    custom_domain = settings.AZ_STORAGE_CUSTOM_HOST
+    endpoint_suffix = settings.AZ_STORAGE_HOST_SUFFIX
     expiration_secs = 600
     overwrite_files = True
 
 
 class AzureStaticStorage(AzureStorage):
-    custom_domain = settings.AZ_STORAGE_HOST
     account_name = settings.AZ_STORAGE_ACCOUNT
     account_key = settings.AZ_STORAGE_KEY
     azure_container = settings.AZ_STATIC_CONTAINER
+    custom_domain = settings.AZ_STORAGE_CUSTOM_HOST
+    endpoint_suffix = settings.AZ_STORAGE_HOST_SUFFIX
     expiration_secs = None
